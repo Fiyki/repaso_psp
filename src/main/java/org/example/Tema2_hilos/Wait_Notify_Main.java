9ca78033277bf0630c6ac6531b1e69a1c9136be5
Daniel Montero Delgado <danmon1b@gmail.com>
2024-03-09 19:20:04 +0100

    Repaso

--- src/main/java/org/example/Tema2_hilos/Wait_Notify_Main.java
+++ src/main/java/org/example/Tema2_hilos/Wait_Notify_Main.java
@@ -5,8 +5,8 @@ public class Wait_Notify_Main {
     public static void main(String[] args) {
         Cola cola = new Cola();
         for (int i = 0; i < 3; i++) {
-            new Productora(cola, "productor " + String.valueOf(i));
-            new Consumidora(cola, "Consumidora " + String.valueOf(i));
+            new Productora(cola, "productor " + i);
+            new Consumidora(cola, "Consumidora " + i);
         }
     }
 }
